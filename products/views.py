392fb674b457e9e2a2e7056d1456d5eb4ab23f5c
yetnetbehailu <yetnet.behailu@hotmail.com>
2022-03-02 12:19:41 +0000

    Added views,templates & url paths for store products & services

--- products/views.py
+++ products/views.py
@@ -1,1 +1,26 @@
 from django.shortcuts import render
+from .models import Product
+
+
+def all_products(request):
+    """
+    View that displays all products including logic to sort & search \
+    queries """
+    products = Product.objects.filter(is_service=False)
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def all_services(request):
+    """ View that displays all of the services"""
+    services = Product.objects.filter(is_service=True)
+
+    context = {
+        'services': services,
+    }
+
+    return render(request, 'products/services.html', context)
