fab7adbbdcf46e2d16ae7ace483013b68549c7a6
yetnetbehailu <yetnet.behailu@hotmail.com>
2022-05-09 16:38:39 +0000

    Add product category link btn/dropdown menu & related filtering logic

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
@@ -7,9 +7,20 @@ def all_products(request):
     View that displays all products including logic to sort & search \
     queries """
     products = Product.objects.filter(is_service=False)
+    # Displays none when hasn't been selected yet eg on page load
+    categories = None
+    # Displays the selected category items
+    if request.GET:
+        if 'category' in request.GET:
+            # splits existing categories into a list at the commas
+            categories = request.GET['category'].split(',')
+            # the __in syntax searches for the name field in category model
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'products': products,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
