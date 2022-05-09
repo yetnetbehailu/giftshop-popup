9b4ac14aa3b24248885b577c79e334a99e35410c
yetnetbehailu <yetnet.behailu@hotmail.com>
2022-05-09 09:13:50 +0000

    Add card structure & content.Provide template context processor for MEDIA_URL

--- giftshop_popup/settings.py
+++ giftshop_popup/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 # required by allauth context processors request
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
