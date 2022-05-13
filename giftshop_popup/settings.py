8e822db75893fd119d94b08d8ab8c216fc64917b
yetnetbehailu <yetnet.behailu@hotmail.com>
2022-05-13 13:42:58 +0000

    Heroku 500 error fix;Updated logic connecting database to server

--- giftshop_popup/settings.py
+++ giftshop_popup/settings.py
@@ -108,16 +108,16 @@ WSGI_APPLICATION = 'giftshop_popup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-if DEBUG:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+       'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
 
 
