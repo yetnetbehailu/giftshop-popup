ae3d6e163fb2957397ba406dc62868c42c1cb9be
yetnetbehailu <yetnet.behailu@hotmail.com>
2022-05-20 09:19:38 +0000

    Added product sorting functionallity

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 
@@ -13,6 +14,8 @@ def all_products(request):
     categories = None
     sub_categories = None
     query = None
+    sort = None
+    direction = None
 
     if request.GET:
         # Displays the selected category items
@@ -41,10 +44,27 @@ def all_products(request):
             # To filter products according to quest pass query to filter method
             products = products.filter(search_query)
 
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'current_categories': categories,
         'search_word': query,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
