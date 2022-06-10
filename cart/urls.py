2e8c4b3a32e18523455aede342e3babe9dc54017
yetnetbehailu <yetnet.behailu@hotmail.com>
2022-06-10 17:29:13 +0000

    Add to cart functionallity constructed & quantity input box added

--- cart/urls.py
+++ cart/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.cart_view, name='cart_view'),
+    path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,19 @@ def cart_view(request):
     """ View that renders the cart contents page """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a quantity off the selected item to the shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+    request.session['cart'] = cart
+
+    return redirect(redirect_url)
