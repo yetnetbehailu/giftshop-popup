38bb5754ff589e2e41e99d2d1347a27eff9ac366
yetnetbehailu <yetnet.behailu@hotmail.com>
2022-06-23 21:15:47 +0000

    Added urls & views for updating & removing items from cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -23,3 +23,31 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
 
     return redirect(redirect_url)
+
+
+def update_cart(request, item_id):
+    """Update the quantity of the specified item to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+    request.session['cart'] = cart
+
+    return redirect(reverse('cart_view'))
+
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+    try:
+        cart = request.session.get('cart', {})
+
+        cart.pop(item_id)
+        request.session['cart'] = cart
+
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
