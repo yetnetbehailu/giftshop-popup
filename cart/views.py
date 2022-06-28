7aac7c0afc28baa3e5e798603492ef597635c57a
yetnetbehailu <yetnet.behailu@hotmail.com>
2022-06-28 09:53:41 +0000

    Added toast messages & styling to the display

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -12,14 +14,19 @@ def cart_view(request):
 def add_to_cart(request, item_id):
     """ Add a quantity off the selected item to the shopping cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated "{product.name}" quantity to \
+            {cart[item_id]}')
     else:
         cart[item_id] = quantity
+        messages.success(request, f'"{product.name}" has been added to your \
+            cart')
     request.session['cart'] = cart
 
     return redirect(redirect_url)
--- giftshop_popup/settings.py
+++ giftshop_popup/settings.py
@@ -84,6 +84,9 @@ TEMPLATES = [
     },
 ]
 
+# Stores messages in session
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
